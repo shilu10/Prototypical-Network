816c5f02ff25708feaede075bf50f6cdb9ee2e4b
Shilu <shilu842003@gmail.com>
2023-11-14 18:53:07 +0530

    updated main.py and protonet.py

--- protonet.py
+++ protonet.py
@@ -15,10 +15,13 @@ import torch.nn.functional as F  # pylint: disable=unused-import
 from google_drive_downloader import GoogleDriveDownloader as gdd
 from torch.utils import tensorboard
 
+
 NUM_INPUT_CHANNELS = 1
 NUM_HIDDEN_CHANNELS = 64
 KERNEL_SIZE = 3
 NUM_CONV_LAYERS = 4
+
+
 
 class ProtoNetNetwork(nn.Module):
     """Container for ProtoNet weights and image-to-latent computation."""
@@ -354,80 +357,11 @@ class ProtoNet:
         print('Saved checkpoint.')
 
 
-def main(args):
-
-    print(args)
-
-    if args.device == "gpu" and torch.backends.mps.is_available() and torch.backends.mps.is_built():
-        DEVICE = "mps"
-    elif args.device == "gpu" and torch.cuda.is_available():
-        DEVICE = "cuda"
-    else:
-        DEVICE = "cpu"
-
-    print("Using device: ", DEVICE)
-
-    log_dir = args.log_dir
-    if log_dir is None:
-        log_dir = f'./logs/protonet/omniglot.way_{args.num_way}.support_{args.num_support}.query_{args.num_query}.lr_{args.learning_rate}.batch_size_{args.batch_size}'  # pylint: disable=line-too-long
-    print(f'log_dir: {log_dir}')
-    writer = tensorboard.SummaryWriter(log_dir=log_dir)
-
-    protonet = ProtoNet(args.learning_rate, log_dir, DEVICE, args.compile, args.backend)
-
-    if args.checkpoint_step > -1:
-        protonet.load(args.checkpoint_step)
-    else:
-        print('Checkpoint loading skipped.')
-
-    if not args.test:
-        num_training_tasks = args.batch_size * (args.num_train_iterations -
-                                                args.checkpoint_step - 1)
-        print(
-            f'Training on tasks with composition '
-            f'num_way={args.num_way}, '
-            f'num_support={args.num_support}, '
-            f'num_query={args.num_query}'
+if __name__ == '__main__':
+    network = ProtoNet(
+            log_dir="logs/",
+            learning_rate=0.001,
+            device='cpu'
         )
-        dataloader_meta_train = get_omniglot_dataloader(
-            'train',
-            args.batch_size,
-            args.num_way,
-            args.num_support,
-            args.num_query,
-            num_training_tasks,
-            args.num_workers
-        )
-        dataloader_meta_val = get_omniglot_dataloader(
-            'val',
-            args.batch_size,
-            args.num_way,
-            args.num_support,
-            args.num_query,
-            args.batch_size * 4,
-            args.num_workers,
-        )
-        protonet.train(
-            dataloader_meta_train,
-            dataloader_meta_val,
-            writer
-        )
-    else:
-        print(
-            f'Testing on tasks with composition '
-            f'num_way={args.num_way}, '
-            f'num_support={args.num_support}, '
-            f'num_query={args.num_query}'
-        )
-        dataloader_test =  get_omniglot_dataloader(
-            'test',
-            1,
-            args.num_way,
-            args.num_support,
-            args.num_query,
-            NUM_TEST_TASKS,
-            args.num_workers
-        )
-        protonet.test(dataloader_test)
 
-
+    print(network)
