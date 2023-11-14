80e77cbe3cd18e483e9bf426d2caa893a400f7cc
Shilu <shilu842003@gmail.com>
2023-11-14 18:46:11 +0530

    added main.py contains main function, which is used for training and testing the model

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main(args):
+import sys
+import argparse
+import os
 
-    print(args)
+import numpy as np
+import torch
+
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+from torch import nn
+import torch.nn.functional as F  # pylint: disable=unused-import
+
+from omniglot import get_omniglot_dataloader
+from protonet import ProtoNet
+
+
+SUMMARY_INTERVAL = 10
+SAVE_INTERVAL = 100
+PRINT_INTERVAL = 10
+VAL_INTERVAL = PRINT_INTERVAL * 5
+NUM_TEST_TASKS = 600
+
+
+def main(args):
 
     if args.device == "gpu" and torch.backends.mps.is_available() and torch.backends.mps.is_built():
         DEVICE = "mps"
@@ -73,5 +95,51 @@ def main(args):
             args.num_workers
         )
         protonet.test(dataloader_test)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Train a ProtoNet!')
+    parser.add_argument('--log_dir', type=str, default=None,
+                        help='directory to save to or load from')
+    parser.add_argument('--num_way', type=int, default=5,
+                        help='number of classes in a task')
+    parser.add_argument('--num_support', type=int, default=1,
+                        help='number of support examples per class in a task')
+    parser.add_argument('--num_query', type=int, default=15,
+                        help='number of query examples per class in a task')
+    parser.add_argument('--learning_rate', type=float, default=0.001,
+                        help='learning rate for the network')
+    parser.add_argument('--batch_size', type=int, default=16,
+                        help='number of tasks per outer-loop update')
+    parser.add_argument('--num_train_iterations', type=int, default=5000,
+                        help='number of outer-loop updates to train for')
+    parser.add_argument('--test', default=False, action='store_true',
+                        help='train or test')
+    parser.add_argument('--checkpoint_step', type=int, default=-1,
+                        help=('checkpoint iteration to load for resuming '
+                              'training, or for evaluation (-1 is ignored)'))
+    parser.add_argument('--num_workers', type=int, default=2, 
+                        help=('needed to specify omniglot dataloader'))
+    parser.add_argument('--compile', action='store_true', default=False)
+    parser.add_argument("--backend", type=str, default="inductor", choices=['inductor', 'aot_eager', 'cudagraphs'])
+    parser.add_argument('--cache', action='store_true')
+    parser.add_argument('--device', type=str, default='cpu')
+
+    args = parser.parse_args()
+
+    if args.cache == True:
+        # Download Omniglot Dataset
+        if not os.path.isdir("./omniglot_resized"):
+            gdd.download_file_from_google_drive(
+                file_id="1iaSFXIYC3AB8q9K_M-oVMa4pmB7yKMtI",
+                dest_path="./omniglot_resized.zip",
+                unzip=True,
+            )
+        assert os.path.isdir("./omniglot_resized")
+    else:
+        main(args)
+                    
+
 
 
